6e0b35cb99a612507c000b4916382d93b1fbd96d
BR88C <br88c@br88c.dev>
2022-04-24 11:54:49 -0400

    Reduce animation length

--- src/App.svelte
+++ src/App.svelte
@@ -186,17 +186,17 @@
 </script>
 
 <main>
-    <Saos animation='slide-in-blurred-top 0.9s cubic-bezier(0.230, 1.000, 0.320, 1.000) both'>
+    <Saos animation='slide-in-blurred-top 0.8s cubic-bezier(0.230, 1.000, 0.320, 1.000) both'>
         <img src='/avatar.webp' alt='avatar' style='height: 25vh; border-radius: 100%;'>
     </Saos>
 
-    <Saos animation='slide-in-blurred-top 1s cubic-bezier(0.230, 1.000, 0.320, 1.000) both'>
+    <Saos animation='slide-in-blurred-top 0.7s cubic-bezier(0.230, 1.000, 0.320, 1.000) both'>
         <h1 style='color: #ff6666; font-size: 4em; font-weight: 700;'>
             Bryce Roethel
         </h1>
     </Saos>
 
-    <Saos animation='slide-in-blurred-top 0.8s cubic-bezier(0.230, 1.000, 0.320, 1.000) both'>
+    <Saos animation='slide-in-blurred-top 0.6s cubic-bezier(0.230, 1.000, 0.320, 1.000) both'>
         {#key phraseIndex}
             <p style='font-size: 2em;'>
                 <b style='animation: slide-in-fwd-center-blurred 0.5s cubic-bezier(0.230, 1.000, 0.320, 1.000);'>{phraseStarts[phraseIndex]}</b> with passion.
@@ -204,11 +204,11 @@
         {/key}
     </Saos>
 
-    <Saos animation='slide-in-blurred-top 0.7s cubic-bezier(0.230, 1.000, 0.320, 1.000) both'>
+    <Saos animation='slide-in-blurred-top 0.5s cubic-bezier(0.230, 1.000, 0.320, 1.000) both'>
         <p style='max-width: 80vh; margin: auto; margin-top: 3vh; font-size: 1.4em; opacity: 0.6;'>Casual programmer that enjoys back-end development. Experienced with Typescript / Node.js, as well as Java. Also dabbling with C.</p>
     </Saos>
 
-    <Saos animation='slide-in-blurred-top 0.6s cubic-bezier(0.230, 1.000, 0.320, 1.000) both'>
+    <Saos animation='slide-in-blurred-top 0.4s cubic-bezier(0.230, 1.000, 0.320, 1.000) both'>
         <a href='https://github.com/BR88C' >
             <svg class="github-svg" style='width: 10vh; margin-top: 4vh;' role='img' viewBox='0 0 24 24'><title>GitHub</title><path style="transition: 0.4s;" fill='#f6f0f0' d='M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12'/></svg>
         </a>
